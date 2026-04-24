e5189ae0fef0ea8543e8bde25eacacb99ba2e3d4
Konstantin <kostyabash@bk.ru>
2026-04-24 22:27:59 +0300

    feat: add Android notification infrastructure

--- app/src/main/java/com/example/general_first_aid_kit/SharedFirstAidKit.kt
+++ app/src/main/java/com/example/general_first_aid_kit/SharedFirstAidKit.kt
@@ -1,11 +1,13 @@
 package com.example.general_first_aid_kit
 
 import android.app.Application
+import com.example.general_first_aid_kit.presentation.service.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class SharedFirstAidKit : Application() {
     override fun onCreate() {
         super.onCreate()
+        NotificationHelper.createChannels(this)
     }
 }
